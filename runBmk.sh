6003f7e283d11666a4cf931645958e77b715bd13
Aditya Agrawal <aagrawal@mips.com>
2023-08-28 10:17:47 +0530

    Updated RunBmk

--- runBmk.sh
+++ runBmk.sh
@@ -1,6 +1,15 @@
+if [ $# -lt 1 ]
+then
+	echo -e "Setting Number of Threads to the maximum."
+	export OMP_NUM_THREADS=$(nproc)
+else
+	echo -e "Setting Number of Threads to the parameter given."
+	export OMP_NUM_THREADS=$1
+fi
+BASE=$(pwd)
 echo -e "Building Point-to-Point Benchmarks"
-./compileBmkP2P.sh /mnt/c/Users/AdityaAgrawal/Research/Artifact/imop/reduciblePhasers/benchmarks/Artifact-P2P TranslatorDriver
+./compileBmkP2P.sh $BASE/reduciblePhasers/benchmarks/Artifact-P2P TranslatorDriver
 echo -e "Building Global Barrier (Without Reduction) Benchmarks"
-./compileBmkNoRed.sh /mnt/c/Users/AdityaAgrawal/Research/Artifact/imop/reduciblePhasers/benchmarks/Artifact-GBNR TranslatorDriverStatic
+./compileBmkNoRed.sh $BASE/reduciblePhasers/benchmarks/Artifact-GBNR TranslatorDriverStatic
 echo -e "Building Global Barrier (With    Reduction) Benchmarks"
-./compileBmkRed.sh /mnt/c/Users/AdityaAgrawal/Research/Artifact/imop/reduciblePhasers/benchmarks/Artifact-GBR TranslatorDriverStatic
+./compileBmkRed.sh $BASE/reduciblePhasers/benchmarks/Artifact-GBR TranslatorDriverStatic
